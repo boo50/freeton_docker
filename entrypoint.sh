1028689959fb21bb3e6dbe9fce1061139206af51
boo50 <30969035+boo50@users.noreply.github.com>
2020-05-19 20:55:16 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+# shellcheck source=env.sh
+. "${SCRIPT_DIR}/env.sh"
+
+echo "INFO: start TON node..."
+echo "INFO: log file = ${TON_WORK_DIR}/node.log"
+
+"${TON_BUILD_DIR}/validator-engine/validator-engine" -C "${TON_WORK_DIR}/etc/ton-global.config.json" --db "${TON_WORK_DIR}/db" > "${TON_WORK_DIR}/node.log"
+
+echo "INFO: start TON node... DONE"
